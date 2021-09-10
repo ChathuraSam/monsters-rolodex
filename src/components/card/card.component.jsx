1226b955a0cbb2b9bdf63c1717572ea043ec1805
chathurasam <chathura.samarajeewa@hcl.com>
2021-09-10 10:30:13 +0530

    background changes and added text search field

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css'
 
 export const Card = (props) => {
     return(
-        <div className="card-container">
+        <div className="card-container" key={props.monster.id}>
             <img alt={props.monster.id} src={`https://robohash.org/${props.monster.id}?size=200x200`}/>
             <h2>{props.monster.name}</h2>
             <p>{props.monster.email}</p>
